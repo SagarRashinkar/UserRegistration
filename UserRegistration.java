e01008923a373c240465ce3507ebc6418486f68a
Sagar <sagarrashinkar2@gmail.com>
2020-10-31 07:13:14 +0530

    ADDED: validate last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -9,14 +9,27 @@ class UserRegistration {
 		System.out.println("Enter first name:");
 		String fname = sc.next();
 		System.out.println(validFirstName(fname));
+		
+		System.out.println("Enter last name:");
+		String lname = sc.next();
+		System.out.println(validLastName(lname));
 	}
 
 	public static String validFirstName(String fname) {
 
 		String pat = "^[A-Z]{1}[a-z]*";
 		if(fname.matches(pat))
-			return "Valid";
+			return "First name is Valid";
 		else
-			return "InValid";
+			return "First name is InValid";
+	}
+	
+	public static String validLastName(String lname) {
+
+		String pat = "^[A-Z]{1}[a-z]*";
+		if(lname.matches(pat))
+			return "Last name is Valid";
+		else
+			return "Last name is InValid";
 	}
 }
